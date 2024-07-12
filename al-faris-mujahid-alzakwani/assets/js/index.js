f087a4b05ebbbfae3e130e2c2a3271449f3f54f0
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-12 17:03:17 +0400

    "Updated HTML files (about, contact, cv, education, experience, index, skills) with new meta tags, title changes, and added name span to nav; updated CSS (index) with new styles for name span; updated JS (index

--- al-faris-mujahid-alzakwani/assets/js/index.js
+++ al-faris-mujahid-alzakwani/assets/js/index.js
@@ -1,16 +1,21 @@
 const userIcon = document.getElementById('userIcon');
 const socialMedia = document.getElementById('socialMedia');
 const smIcons = document.getElementById('smIcons');
+const name = document.querySelector('span.name');
 
 userIcon.addEventListener('click', () => {
   if(socialMedia.style.opacity === '1'){
     userIcon.style.boxShadow = 'none';
     userIcon.style.transform = 'scale(0.95)';
+    name.style.opacity = '0';
+    name.style.transform = 'translate(-60px, 28.5px)';
     socialMedia.style.opacity = '0';
     socialMedia.style.transform = 'translateX(-90px)';
   } else{
         userIcon.style.boxShadow = '0 0 20px rgba(150, 150, 150, 0.8)';
         userIcon.style.transform = 'scale(1.05)';
+        name.style.opacity = '1';
+        name.style.transform = 'translate(20px, 28.5px)';
         socialMedia.style.opacity = '1';
         socialMedia.style.transform = 'translateX(26px)';
     }

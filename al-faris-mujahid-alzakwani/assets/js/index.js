89e1d643ee8a7aeb40dc4d3c1a97804784b0eedc
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2024-07-14 09:39:36 +0400

    add an extra image beside user icon

--- al-faris-mujahid-alzakwani/assets/js/index.js
+++ al-faris-mujahid-alzakwani/assets/js/index.js
@@ -1,4 +1,5 @@
-const userIcon = document.getElementById('userIcon');
+const userIcon = document.getElementById('userIcon-1');
+const userIcon2 = document.getElementById('userIcon-2');
 const socialMedia = document.getElementById('socialMedia');
 const smIcons = document.getElementById('smIcons');
 const name = document.querySelector('span.name');
@@ -6,6 +7,8 @@ const name = document.querySelector('span.name');
 
 userIcon.addEventListener('click', () => {
   if(socialMedia.style.opacity === '1'){
+    userIcon2.style.transform = 'translate(-75px, 7px)';
+    userIcon2.style.boxShadow = 'none';
     userIcon.style.boxShadow = 'none';
     userIcon.style.transform = 'scale(0.95)';
     name.style.opacity = '0';
@@ -13,10 +16,12 @@ userIcon.addEventListener('click', () => {
     socialMedia.style.opacity = '0';
     socialMedia.style.transform = 'translateX(-90px)';
   } else{
+        userIcon2.style.transform = 'translate(20px, 7px)';
+        userIcon2.style.boxShadow = '0 0 20px rgba(150, 150, 150, 0.5)';
         userIcon.style.boxShadow = '0 0 20px rgba(150, 150, 150, 0.8)';
         userIcon.style.transform = 'scale(1.05)';
         name.style.opacity = '1';
-        name.style.transform = 'translate(20px, 28.5px)';
+        name.style.transform = 'translate(100px, 28.5px)';
         socialMedia.style.opacity = '1';
         socialMedia.style.transform = 'translateX(26px)';
     }
